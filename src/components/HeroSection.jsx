d28e90efcd1f9bff96e33cbb5e3acd2746763a60
Lucas <luema94@gmail.com>
2025-12-07 23:03:37 -0300

    se sube actualizacion al front

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import lucas from "../assets/lucas.png"
 
 export const HeroSection = ()=>{
     return (
-        <section id="home" className="min-h-screen container mx-auto flex flex-col justify-center items-center -pt-20 xl:-pt-20 mt-5 xl:flex-row xl:mt-0 relative">
+        <section id="home" className="min-h-screen container mx-auto flex flex-col justify-center items-center xl:-pt-20 -mt-15 xl:flex-row xl:mt-0 relative">
         <div className="text-center px-4">
         <h1 className="text-white text-2x1 font-bold xl:text-4xl">Software Developer</h1>
         <p className=" text-indigo-700 text-4xl xl:text-7xl">Hi, I'm Lucas Caño.</p>
