ea6c7a5c1366a2e86ce64daead7e8acaa78e4a7e
Lucas <luema94@gmail.com>
2025-12-12 19:04:36 -0300

    se sube docu front

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -94,9 +94,9 @@ export const Projects = ()=>{
                 currentProjects.map((item, i)=>{
 
                   return (
-                    <div onClick={()=>handleSelected(item.name)} key={i} className="relative w-90 h-70 sm:w-115 sm:h-90 flex items-center justify-center rounded-md overflow-hidden hover:shadow-[0_0_15px_rgba(255,255,255,0.3)] transition duration-300">
+                    <div onClick={()=>handleSelected(item.name)} key={i} className="relative w-80 h-60 sm:w-115 sm:h-90 flex items-center justify-center rounded-md overflow-hidden hover:shadow-[0_0_15px_rgba(255,255,255,0.3)] transition duration-300">
                       <img src={item.image} className="w-full h-full object-contain object-center rounded-md cursor-pointer hover:grayscale hover:duration-300 border"/>
-                      <div className={`absolute inset-0 bg-black/0 hover:bg-black/50 backdrop-blur-sm flex items-center justify-center text-white text-center opacity-0 hover:opacity-100 transition duration-300  ${item.name === selected ? "opacity-100 bg-black/60" : "opacity-0"}
+                      <div className={`text-[14px] absolute inset-0 bg-black/0 hover:bg-black/50 backdrop-blur-sm flex items-center justify-center text-white text-center opacity-0 hover:opacity-100 transition duration-300  ${item.name === selected ? "opacity-100 bg-black/60" : "opacity-0"}
       group-hover:opacity-100 group-hover:bg-black/60`}>
                       {item.description}
                       </div>
