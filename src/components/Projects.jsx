cf83fa6e1e3926d647d90e8de700e6ce97b07ec2
Lucas <luema94@gmail.com>
2025-12-14 22:10:55 -0300

    se añaden funciones al front

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,26 +13,32 @@ export const Projects = ()=>{
       id: 1,
       name: "Videogame",
       image: videogamePage,
-      description: "En este proyecto se creo una spa de videojuegos en la cual buscamos el que queremos encontrar de manera ascendente y descendente y podemos agregar alguno que no este en el catalogo"
+      gitHub: "https://github.com/Lucas0894/AppVideogame",
+      description: "Single Page Application (SPA) desarrollada para la exploración y gestión de videojuegos. Permite buscar títulos, ordenarlos alfabéticamente o por puntuación, y agregar videojuegos a una biblioteca personalizada con información como nombre e imagen."
 
     },
     {
       id: 2,
       name: "foodProject",
       image: foodPage,
-      description: "En este proyecto se creo una spa de videojuegos en la cual buscamos el que queremos encontrar de manera ascendente y descendente y podemos agregar alguno que no este en el catalogo"
+      gitHub: "https://github.com/Lucas0894/deployFood",
+      description: "Single Page Application (SPA) enfocada en el descubrimiento y organización de recetas de cocina. Ofrece búsqueda interactiva, filtros de ordenamiento por nombre y valoración, y la posibilidad de guardar recetas en una colección personal con sus datos esenciales, como imagen e ingredientes."
     },
     {
       id: 3,
       name: "reparoProject",
       image: reparo,
-      description: "En este proyecto se creo una spa de videojuegos en la cual buscamos el que queremos encontrar de manera ascendente y descendente y podemos agregar alguno que no este en el catalogo"
+      page: "https://reparoio.vercel.app/",
+      gitHub: "https://github.com/Joel301/reparo_io_FrontEnd",
+      description: "Reparo.io es un ecommerce desarrollado en equipo (8 personas) bajo metodología Scrum, que conecta usuarios con técnicos especializados. Incluye autenticación, rol administrador y pasarela de pago integrada con Mercado Pago."
     },
     {
       id: 4,
       name: "portfolio",
       image: portfolio,
-      description: "En este proyecto se creo una spa de videojuegos en la cual buscamos el que queremos encontrar de manera ascendente y descendente y podemos agregar alguno que no este en el catalogo"
+      page: "https://lucascano.vercel.app/",
+      gitHub: "https://github.com/Lucas0894/portfolio",
+      description: "Portfolio personal desarrollado para presentar mis proyectos, información sobre mí y enlaces a mis perfiles profesionales (GitHub y LinkedIn). Incluye una sección de contacto vía correo electrónico, descarga de CV, y fue construido con HTML, CSS y JavaScript, utilizando React y Tailwind CSS en el frontend, Express en el backend y Vercel para el despliegue."
     }
    ];
 
@@ -95,10 +101,20 @@ export const Projects = ()=>{
                   return (
                     <div onClick={()=>handleSelected(item.name)} key={i} className={`relative w-70 h-50 sm:w-115 sm:h-90 flex items-center justify-center rounded-md overflow-hidden hover:shadow-[0_0_15px_rgba(255,255,255,0.3)] cursor-pointer ${item.name === selected? "shadow-[0_0_15px_rgba(255,255,255,0.3)]": ""} transition duration-300`}>
                       <img src={item.image} className="w-full h-full object-contain object-center rounded-md cursor-pointer hover:grayscale hover:duration-300 border"/>
-                      <div className={`text-[14px] absolute inset-0 bg-black/0 p-2 hover:bg-black/50 backdrop-blur-sm flex items-center justify-center text-white text-center opacity-0 hover:opacity-100 transition duration-300  ${item.name === selected ? "opacity-100 bg-black/60" : "opacity-0"}
+                      <div className={`text-[14px] absolute inset-0 bg-black/0 p-2 hover:bg-black/50 backdrop-blur-sm flex flex-col items-center justify-center text-white text-center opacity-0 hover:opacity-100 transition duration-300  ${item.name === selected ? "opacity-100 bg-black/60" : "opacity-0"}
       group-hover:opacity-100 group-hover:bg-black/60`}>
                       {item.description}
+                      <button className="p-2 m-2 bg-indigo-900 rounded-3xl cursor-pointer transition shadow-[0_0_15px_rgba(255,255,255,0.3)] transform hover:scale-110"> 
+                      <a href={item.gitHub}>Link Repositorio</a>
+                      </button>
+                      {
+                        item.page?
+                        <button className="p-2 m-2 bg-indigo-900 rounded-3xl cursor-pointer shadow-[0_0_15px_rgba(255,255,255,0.3)] transition transform hover:scale-110"> 
+                        <a href={item.gitHub}>Sitio Web</a>
+                        </button>: ""
+                      }
                       </div>
+                      
                       </div>
     
                   )
