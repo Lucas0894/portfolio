839577bba5d9742ca3eef420513e53e55ebc752f
Lucas <luema94@gmail.com>
2025-12-16 21:28:30 -0300

    se suben funciones al front

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@
 export const Footer = ()=>{
     return (
         <>
-        <section className="flex items-center justify-center m-5 border-t-gray-800 border-1">
+        <section className="flex items-center justify-center m-2 border-t-gray-800 border-1">
             <div>
             <div className="flex gap-5 m-3 items-center justify-center">
-                <a className="text-gray-400 font-bold cursor-pointer hover:text-white">Home</a>
-                <a className="text-gray-400 font-bold cursor-pointer hover:text-white">About</a>
-                <a className="text-gray-400 font-bold cursor-pointer hover:text-white">Projects</a>
-                <a className="text-gray-400 font-bold cursor-pointer hover:text-white">Contact</a>
+                <a href="#home" className="text-gray-400 font-bold cursor-pointer hover:text-white">Home</a>
+                <a href="#about" className="text-gray-400 font-bold cursor-pointer hover:text-white">About</a>
+                <a href="#projects" className="text-gray-400 font-bold cursor-pointer hover:text-white">Projects</a>
+                <a href="#contact" className="text-gray-400 font-bold cursor-pointer hover:text-white">Contact</a>
             </div>
             <div className="text-gray-400 text-[12px] xl:text-[15px]">
                 2025 Designed & Developed by Lucas Caño. All rights reserved
