b8e4f6508193ee48a8b510e2bf706ae24d50a159
Lucas <luema94@gmail.com>
2025-12-22 20:28:37 -0300

    se modifico una carpeta del front

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ import { PacmanLoader} from "react-spinners"
 export const Form = ()=>{
     const API_URL = import.meta.env.VITE_API_URL;
     const [sending, setSending] = useState(false)
+    const [errors, setErrors] = useState({})
 
     const [form, setForm] = useState({
         nombre: "",
@@ -14,17 +15,48 @@ export const Form = ()=>{
         mensaje: ""
     })
 
+    const handleErrors = ()=>{
+        const newErrors ={};
+        if(!form.nombre.trim()){
+            newErrors.nombre = "El nombre es obligatorio"
+        }
+        if(!form.email.trim()){
+            newErrors.email = "El email es obligatorio"
+        } else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email)){
+            newErrors.email = "El mail no es valido"
+        }
+        if(!form.asunto.trim()){
+            newErrors.asunto = "El asunto es obligatorio"
+        }
+        if(!form.mensaje.trim()){
+            newErrors.mensaje = "El mensaje es obligatorio"
+        }
+        return newErrors
+
+    }
+
     function handleChange(e){
         
         setForm({
             ...form,
             [e.target.name]: e.target.value
         })
+
+        setErrors({
+            ...errors,
+            [e.target.name]: ""
+        })
+
         console.log(form)
     }
 
     const handleSubmit = async (e)=>{
         e.preventDefault();
+        const validationForm = handleErrors();
+        setErrors(validationForm)
+        if(Object.keys(validationForm).length > 0){
+            return;
+        }
         setSending(true)
         try {
             const res = await fetch(`https://backend-portfolio-7c6b.onrender.com/send`, {
@@ -38,6 +70,7 @@ export const Form = ()=>{
             if(data.success){
                 alert("correo enviado correctamente")
                 setForm({nombre: "", email: "", asunto: "", mensaje: ""})
+                setErrors({})
             }else{
                 alert("el mensaje no se envio")
             }
@@ -57,9 +90,13 @@ export const Form = ()=>{
     <h1 className="text-white text-center font-bold text-2xl mt-10">Contact</h1> 
     <form id="contact" className="flex flex-col gap-4 items-center justify-center backdrop-blur-sm mt-8 p-5" onSubmit={handleSubmit}>
         <input name="nombre" className="w-75 sm:w-160 bg-white rounded-md p-2" value={form.nombre} onChange={(e)=>handleChange(e)} placeholder="Nombre y Apellido" />
+        {errors.nombre && <p className="text-red-500 text-sm">{errors.nombre}</p>}
         <input name="email" className="w-75 sm:w-160 bg-white rounded-md p-2" value={form.email} onChange={(e)=>handleChange(e)} placeholder="E-mail" />
+        {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
         <input name="asunto" className="w-75 sm:w-160 bg-white rounded-md p-2" value={form.asunto} onChange={(e)=>handleChange(e)} placeholder="Asunto" />
+        {errors.asunto && <p className="text-red-500 text-sm">{errors.asunto}</p>}
         <textarea name="mensaje" className="w-75 sm:w-160 h-60 bg-white rounded-md p-2" value={form.mensaje} onChange={(e)=>handleChange(e)} placeholder="Mensaje" />
+        {errors.mensaje && <p className="text-red-500 text-sm">{errors.mensaje}</p>}
         <button className="text-white font-bold bg-indigo-600 p-2 w-75 sm:w-160 rounded-md cursor-pointer hover:bg-indigo-700 hover:shadow-[0_0_20px_4px_rgba(99,102,241,0.8)] transition transform hover:scale-105 duration-300">Send</button>
         {
             sending?
