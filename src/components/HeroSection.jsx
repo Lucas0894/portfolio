f19771ebcd603dad6fb07f3baa6e6cdc5bd70dea
Lucas <luema94@gmail.com>
2026-03-26 19:28:51 -0300

    se sube actualizacion

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,8 @@ import  LucasC_CV  from "../assets/Lucas_Caño_CV_Ingles.pdf"
 
 export const HeroSection = ()=>{
 
-    
+
+
     return (
         <motion.div
         initial={{ opacity: 0, y: 50 }}
@@ -19,7 +20,7 @@ export const HeroSection = ()=>{
         <p className=" text-white text-4xl xl:text-7xl font-semibold">Hola, soy {" "}<span className="text-indigo-500">Lucas Caño.</span></p>
         <p className="text-white mt-6 mx-20">Desarrollador Frontend especializado en <span className="font-bold">React</span> y <span className="font-bold">JavaScript</span>, enfocado en crear interfaces web claras, funcionales y responsivas con foco en UX.</p>
         <div className="xl:mt-20">
-                <a className="relative bg-[#202020] group inline-block rounded-2xl xl:m-10 p-4 m-6  shadow-[0_6px_16px_rgba(0,0,0,0.45),0_28px_55px_rgba(0,0,0,0.85)] cursor-pointer transition transform hover:scale-110 hover:shadow-[0_6px_16px_rgba(99,102,241,0.35),0_28px_55px_rgba(99,102,241,0.55)]" href="https://github.com/Lucas0894">
+                <a className="relative bg-[#202020] group inline-block rounded-2xl xl:m-10 p-4 m-4  shadow-[0_6px_16px_rgba(0,0,0,0.45),0_28px_55px_rgba(0,0,0,0.85)] cursor-pointer transition transform hover:scale-110 hover:shadow-[0_6px_16px_rgba(99,102,241,0.35),0_28px_55px_rgba(99,102,241,0.55)]" href="https://github.com/Lucas0894">
                 <div className="absolute text-white opacity-0 -top-16 bg-gray-900 group-hover:opacity-100 pointer-events-none transition-opacity left-1/2 -translate-x-1/2 p-2 rounded-full duration-200" >Github<div className="absolute left-1/2 -translate-x-1/2 w-2 h-2 -bottom-1 rotate-45 bg-gray-900" /></div>
                 <PiGithubLogoFill size={50} className="text-white" />
                 </a>
