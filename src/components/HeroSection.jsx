3cfe5cda09fd07e75ed2ba4d3026baaee518b5a7
Lucas <luema94@gmail.com>
2026-03-26 20:52:16 -0300

    se sube actualizacion

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,13 +8,7 @@ import { TypeAnimation } from "react-type-animation";
 import { useState, useEffect } from "react";
 
 export const HeroSection = ()=>{
-     const [loopKey, setLoopKey] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setLoopKey(prev => prev + 1), 4000);
-    return () => clearTimeout(timer);
-  }, [loopKey]);
-
+    
     return (
         <motion.div
         initial={{ opacity: 0, y: 50 }}
@@ -22,7 +16,7 @@ export const HeroSection = ()=>{
         transition={{ duration: 0.8 }}>
         <section id="home" className="min-h-screen container mx-auto flex flex-col justify-center items-center xl:-pt-20 mt-5 xl:flex-row xl:mt-0 relative">
         <div className="text-center px-4">
-        <h1 className="text-white text-2xl xl:text-4xl"><TypeAnimation key={loopKey} sequence={["Frontend Developer · ",1500, "", 500]} speed={50}repeat={0}/><span className="text-indigo-400">React</span></h1>
+        <h1 className="text-white text-2xl xl:text-4xl"><TypeAnimation sequence={["Frontend Developer · ",1500, "", 500]} speed={50}repeat={Infinity}/><span className="text-indigo-400">React</span></h1>
         <p className=" text-white text-4xl xl:text-7xl font-semibold">Hola, soy {" "}<span className="text-indigo-500">Lucas Caño.</span></p>
         <p className="text-white mt-6 mx-20">Desarrollador Frontend especializado en <span className="font-bold">React</span> y <span className="font-bold">JavaScript</span>, enfocado en crear interfaces web claras, funcionales y responsivas con foco en UX.</p>
         <div className="xl:mt-20">
