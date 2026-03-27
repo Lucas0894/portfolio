a87eba1e374ac0c637ab9103cd68b5c4938dc867
Lucas <luema94@gmail.com>
2026-03-26 21:12:10 -0300

    se sube actualizacion

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,7 +16,7 @@ export const HeroSection = ()=>{
         <section id="home" className="min-h-screen container mx-auto flex flex-col justify-center items-center xl:-pt-20 mt-5 xl:flex-row xl:mt-0 relative">
         <div className="text-center px-4">
         <h1 className="text-white text-2xl xl:text-4xl"><TypeAnimation sequence={["Frontend Developer · ",1500, "", 500]} speed={50}repeat={Infinity}/><span className="text-indigo-400">React</span></h1>
-        <p className=" text-white text-4xl xl:text-7xl font-semibold">Hola, soy {" "}<span className="text-indigo-500">Lucas Caño.</span></p>
+        <p className=" text-white text-4xl xl:text-7xl font-semibold">Hola, soy {" "}<span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-indigo-400 to-blue-400">Lucas Caño.</span></p>
         <p className="text-white mt-6 mx-20">Desarrollador Frontend especializado en <span className="font-bold">React</span> y <span className="font-bold">JavaScript</span>, enfocado en crear interfaces web claras, funcionales y responsivas con foco en UX.</p>
         <div className="xl:mt-20">
                 <a className="relative bg-[#202020] group inline-block rounded-2xl xl:m-10 p-4 m-4  shadow-[0_6px_16px_rgba(0,0,0,0.45),0_28px_55px_rgba(0,0,0,0.85)] cursor-pointer transition transform hover:scale-110 hover:shadow-[0_6px_16px_rgba(99,102,241,0.35),0_28px_55px_rgba(99,102,241,0.55)]" href="https://github.com/Lucas0894">
@@ -34,7 +34,7 @@ export const HeroSection = ()=>{
         </div>
         </div>
         <div className="blob-wrapper w-60 h-60 xl:w-110 xl:h-110">
-                <img alt="img Lucas" src={lucas} className="blob-image object-cover" />
+                <img style={{ boxShadow: `0 0 10px #3b82f6,0 0 20px #3b82f6,0 0 30px #5c6ac4,0 0 40px #5c6ac4`}} alt="img Lucas" src={lucas} className="blob-image object-cover" />
         </div>
         </section>
         </motion.div>
