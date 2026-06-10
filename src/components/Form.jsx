1274eb26c06112cc64689c03a7e2a5ec6fd20213
Lucas <luema94@gmail.com>
2026-06-09 22:11:43 -0300

    fix: se arreglan estilos del form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { RingLoader } from "react-spinners"
 import { motion } from "framer-motion";
-import { User } from "lucide-react"
+import { User, Mail, Tag, MessageCircle } from "lucide-react"
 
 export const Form = ()=>{
     const API_URL = import.meta.env.VITE_API_URL;
@@ -92,17 +92,27 @@ export const Form = ()=>{
     <h1 className="text-zinc-300 text-center font-bold text-2xl mt-10">Contacto</h1> 
     <div className="relative bg-[#202020] shadow-[0_6px_16px_rgba(0,0,0,0.45),0_28px_55px_rgba(0,0,0,0.85)] mt-10 border border-white/5 rounded-3xl sm:p-5">
     <form id="contact" className="flex flex-col gap-4 items-center justify-center backdrop-blur-sm mt-8 p-5" onSubmit={handleSubmit}>
-        <input name="nombre" className="w-75 sm:w-160 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.nombre} onChange={(e)=>handleChange(e)} placeholder="Nombre y Apellido" />
+        <div className="relative w-75 sm:w-160">
+        <input name="nombre" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.nombre} onChange={(e)=>handleChange(e)} placeholder="Nombre y Apellido" />
+        <User className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
+        </div>
         {errors.nombre && <p className="text-red-500 text-sm">{errors.nombre}</p>}
-        <input name="email" className="w-75 sm:w-160 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.email} onChange={(e)=>handleChange(e)} placeholder="E-mail" />
+        <div className="relative w-75 sm:w-160">
+        <input name="email" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.email} onChange={(e)=>handleChange(e)} placeholder="E-mail" />
+        <Mail className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
+        </div>
         {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
-        <input name="asunto" className="w-75 sm:w-160 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.asunto} onChange={(e)=>handleChange(e)} placeholder="Asunto" />
+        <div className="relative w-75 sm:w-160">
+        <input name="asunto" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.asunto} onChange={(e)=>handleChange(e)} placeholder="Asunto" />
+        <Tag className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
+        </div>
         {errors.asunto && <p className="text-red-500 text-sm">{errors.asunto}</p>}
-        <textarea name="mensaje" className="resize-none text-zinc-200 w-75 sm:w-160 h-60 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.mensaje} onChange={(e)=>handleChange(e)} placeholder="Mensaje" />
+        <div className="relative w-75 sm:w-160">
+        <textarea name="mensaje" className="resize-none text-zinc-200 w-full pl-10 h-60 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.mensaje} onChange={(e)=>handleChange(e)} placeholder="Mensaje" />
+        <MessageCircle className="absolute left-2 top-2 text-indigo-700" />
+        </div>
         {errors.mensaje && <p className="text-red-500 text-sm">{errors.mensaje}</p>}
-        <button className="text-zinc-300 font-bold bg-gradient-to-r
-from-violet-600
-to-indigo-800 p-2 w-75 sm:w-160 rounded-md cursor-pointer hover:bg-indigo-700 hover:shadow-[0_0_20px_4px_rgba(99,102,241,0.8)] transition transform hover:scale-105 duration-300">Enviar mensaje</button>
+        <button className="text-zinc-300 font-bold bg-gradient-to-r from-violet-600 to-indigo-800 p-2 w-75 sm:w-160 rounded-md cursor-pointer hover:bg-indigo-700 hover:shadow-[0_0_20px_4px_rgba(99,102,241,0.8)] transition transform hover:scale-105 duration-300">Enviar mensaje</button>
         {
             sending?
             <div className={`absolute inset-0 bg-black/70 backdrop-blur-md flex items-center justify-center rounded-md z-10`}>
