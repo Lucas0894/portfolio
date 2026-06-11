8093ae7a5e85816cd659eccee60db17b78c152c2
Lucas <luema94@gmail.com>
2026-06-10 21:35:54 -0300

    fix: se arreglaron estilos de typografia

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -93,22 +93,22 @@ export const Form = ()=>{
     <div className="relative bg-[#202020] shadow-[0_6px_16px_rgba(0,0,0,0.45),0_28px_55px_rgba(0,0,0,0.85)] mt-10 border border-white/5 rounded-3xl sm:p-5">
     <form id="contact" className="flex flex-col gap-4 items-center justify-center backdrop-blur-sm mt-8 p-5" onSubmit={handleSubmit}>
         <div className="relative w-75 sm:w-160">
-        <input name="nombre" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.nombre} onChange={(e)=>handleChange(e)} placeholder="Nombre y Apellido" />
+        <input name="nombre" className="w-full pl-10 text-zinc-200 bg-zinc-800 transition-all duration-300 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.nombre} onChange={(e)=>handleChange(e)} placeholder="Nombre y Apellido" />
         <User className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
         </div>
         {errors.nombre && <p className="text-red-500 text-sm">{errors.nombre}</p>}
         <div className="relative w-75 sm:w-160">
-        <input name="email" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.email} onChange={(e)=>handleChange(e)} placeholder="E-mail" />
+        <input name="email" className="w-full pl-10 text-zinc-200 bg-zinc-800 transition-all duration-300 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.email} onChange={(e)=>handleChange(e)} placeholder="E-mail" />
         <Mail className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
         </div>
         {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
         <div className="relative w-75 sm:w-160">
-        <input name="asunto" className="w-full pl-10 text-zinc-200 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.asunto} onChange={(e)=>handleChange(e)} placeholder="Asunto" />
+        <input name="asunto" className="w-full pl-10 text-zinc-200 bg-zinc-800 transition-all duration-300 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.asunto} onChange={(e)=>handleChange(e)} placeholder="Asunto" />
         <Tag className="absolute left-2 top-1/2 -translate-y-1/2 text-indigo-700" />
         </div>
         {errors.asunto && <p className="text-red-500 text-sm">{errors.asunto}</p>}
         <div className="relative w-75 sm:w-160">
-        <textarea name="mensaje" className="resize-none text-zinc-200 w-full pl-10 h-60 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.mensaje} onChange={(e)=>handleChange(e)} placeholder="Mensaje" />
+        <textarea name="mensaje" className="resize-none text-zinc-200 transition-all duration-300 w-full pl-10 h-60 bg-zinc-800 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-indigo-700" value={form.mensaje} onChange={(e)=>handleChange(e)} placeholder="Mensaje" />
         <MessageCircle className="absolute left-2 top-2 text-indigo-700" />
         </div>
         {errors.mensaje && <p className="text-red-500 text-sm">{errors.mensaje}</p>}
